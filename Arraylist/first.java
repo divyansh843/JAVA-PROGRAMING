4f7edd352b65dc6290eb87f76f640578bf7e3bde
Divyaansh Kumar <132772094+divyansh843@users.noreply.github.com>
2025-12-10 04:57:42 +0000

    arraylist file

---
+++ Arraylist/first.java
@@ -0,0 +1,19 @@
+package Arraylist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+         ArrayList<Integer> al = new ArrayList<>(6);
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            int ele = sc.nextInt();
+            al.add(ele);    
+        }
+
+        System.out.print(al);
+        
+    }
+    
+}
